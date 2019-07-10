9cc469ec618ec8bce9a070b7b8696e6720f0d33d
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2019-07-10 09:23:01 -0300

    Build script improved

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 BASEDIR=$(dirname $(realpath "$0")) # https://stackoverflow.com/a/55472432/1657502
+USERNAME=$(whoami)
 sudo rm -rf /tmp/myimage
 sudo kiwi-ng --type iso system build --description $BASEDIR --target-dir /tmp/myimage
+sudo chmod 777 /tmp/myimage/Linux-Kamarada*
+sudo chown $USERNAME /tmp/myimage/Linux-Kamarada*
+sudo mv /tmp/myimage/Linux-Kamarada* $BASEDIR
