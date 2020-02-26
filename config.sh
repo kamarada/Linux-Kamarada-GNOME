52f52cf0af09b78aa88f99919a4ec075552ac144
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2020-02-26 16:35:49 -0300

    Upgrading from 15.1 to 15.2

--- config.sh
+++ config.sh
@@ -61,18 +61,18 @@ sed -i -e 's,^\(.*pam_gnome_keyring.so.*\),#\1,'  /etc/pam.d/common-auth-pc
 baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER_AUTOLOGIN linux
 
 # Official repositories
-# (as found in http://download.opensuse.org/distribution/leap/15.1/repo/oss/control.xml)
+# (as found in http://download.opensuse.org/distribution/leap/15.2/repo/oss/control.xml)
 rm /etc/zypp/repos.d/*.repo
-zypper addrepo -f -K -n "Main Update Repository" http://download.opensuse.org/update/leap/15.1/oss/ repo-update
-zypper addrepo -f -K -n "Update Repository (Non-Oss)" http://download.opensuse.org/update/leap/15.1/non-oss/ repo-update-non-oss
-zypper addrepo -f -K -n "Main Repository" http://download.opensuse.org/distribution/leap/15.1/repo/oss/ repo-oss
-zypper addrepo -f -K -n "Non-OSS Repository" http://download.opensuse.org/distribution/leap/15.1/repo/non-oss/ repo-non-oss
-zypper addrepo -d -K -n "Debug Repository" http://download.opensuse.org/debug/distribution/leap/15.1/repo/oss/ repo-debug
-zypper addrepo -d -K -n "Debug Repository (Non-OSS)" http://download.opensuse.org/debug/distribution/leap/15.1/repo/non-oss/ repo-debug-non-oss
-zypper addrepo -d -K -n "Update Repository (Debug)" http://download.opensuse.org/debug/update/leap/15.1/oss repo-debug-update
-zypper addrepo -d -K -n "Update Repository (Debug, Non-OSS)" http://download.opensuse.org/debug/update/leap/15.1/non-oss/ repo-debug-update-non-oss
-zypper addrepo -d -K -n "Source Repository" http://download.opensuse.org/source/distribution/leap/15.1/repo/oss/ repo-source
-zypper addrepo -d -K -n "Source Repository (Non-OSS)" http://download.opensuse.org/source/distribution/leap/15.1/repo/non-oss/ repo-source-non-oss
+zypper addrepo -f -K -n "Main Update Repository" http://download.opensuse.org/update/leap/15.2/oss/ repo-update
+zypper addrepo -f -K -n "Update Repository (Non-Oss)" http://download.opensuse.org/update/leap/15.2/non-oss/ repo-update-non-oss
+zypper addrepo -f -K -n "Main Repository" http://download.opensuse.org/distribution/leap/15.2/repo/oss/ repo-oss
+zypper addrepo -f -K -n "Non-OSS Repository" http://download.opensuse.org/distribution/leap/15.2/repo/non-oss/ repo-non-oss
+zypper addrepo -d -K -n "Debug Repository" http://download.opensuse.org/debug/distribution/leap/15.2/repo/oss/ repo-debug
+zypper addrepo -d -K -n "Debug Repository (Non-OSS)" http://download.opensuse.org/debug/distribution/leap/15.2/repo/non-oss/ repo-debug-non-oss
+zypper addrepo -d -K -n "Update Repository (Debug)" http://download.opensuse.org/debug/update/leap/15.2/oss repo-debug-update
+zypper addrepo -d -K -n "Update Repository (Debug, Non-OSS)" http://download.opensuse.org/debug/update/leap/15.2/non-oss/ repo-debug-update-non-oss
+zypper addrepo -d -K -n "Source Repository" http://download.opensuse.org/source/distribution/leap/15.2/repo/oss/ repo-source
+zypper addrepo -d -K -n "Source Repository (Non-OSS)" http://download.opensuse.org/source/distribution/leap/15.2/repo/non-oss/ repo-source-non-oss
 
 # openSUSE Bug 984330 overlayfs requires AppArmor attach_disconnected flag
 # https://bugzilla.opensuse.org/show_bug.cgi?id=984330
