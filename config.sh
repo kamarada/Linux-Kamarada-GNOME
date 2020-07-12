6a70a8257d51f5332f64e29e792e22475f2a531f
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2020-07-11 23:46:52 -0300

    Production already can use OSDN mirrors

--- config.sh
+++ config.sh
@@ -86,12 +86,11 @@ zypper mr -r repo-update-non-oss
 
 # Kamarada repository
 # See: https://github.com/kamarada/Linux-Kamarada-GNOME/wiki/Mirrors
-#KAMARADA_MIRROR="https://osdn.mirror.constant.com/storage/g/k/ka/kamarada/15.1/openSUSE_Leap_15.1/"
-#if [[ "$kiwi_profiles" == *"pt_BR"* ]]
-#then
-#    KAMARADA_MIRROR="http://c3sl.dl.osdn.jp/storage/g/k/ka/kamarada/15.1/openSUSE_Leap_15.1/"
-#fi
-KAMARADA_MIRROR="http://download.opensuse.org/repositories/home:/kamarada:/15.2/openSUSE_Leap_15.2/"
+KAMARADA_MIRROR="https://osdn.mirror.constant.com/storage/g/k/ka/kamarada/15.2/openSUSE_Leap_15.2/"
+if [[ "$kiwi_profiles" == *"pt_BR"* ]]
+then
+    KAMARADA_MIRROR="http://c3sl.dl.osdn.jp/storage/g/k/ka/kamarada/15.2/openSUSE_Leap_15.2/"
+fi
 zypper addrepo -f -K -n "Linux Kamarada" "$KAMARADA_MIRROR" kamarada
 
 # openSUSE Bug 984330 overlayfs requires AppArmor attach_disconnected flag
