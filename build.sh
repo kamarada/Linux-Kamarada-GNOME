ff41b2f88f36f846479f002335af565bd1d6f836
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2020-09-09 00:35:49 -0300

    Build script using the user home, because /tmp is now tmpfs (resides in memory)

--- build.sh
+++ build.sh
@@ -2,19 +2,21 @@
 set -e
 
 USERNAME=$(whoami)
+USERHOME=$HOME
 THIS_SCRIPT=$(realpath "$0")
 
 if [[ $UID -gt 0 ]] ; then
-    sudo sh $THIS_SCRIPT $USERNAME # https://askubuntu.com/a/719582/560233
+    sudo sh $THIS_SCRIPT $USERNAME $USERHOME # https://askubuntu.com/a/719582/560233
     exit
 fi
 
 USERNAME=$1
+USERHOME=$2
 BASEDIR=$(dirname $THIS_SCRIPT) # https://stackoverflow.com/a/55472432/1657502
 
-rm -rf /tmp/myimage
-kiwi-ng --type iso --profile=pt_BR system build --description $BASEDIR --target-dir /tmp/myimage
-kiwi-ng result bundle --target-dir /tmp/myimage --id pt_BR-Build1.1 --bundle-dir $BASEDIR
+rm -rf $USERHOME/tmp-kamarada
+kiwi-ng --type iso --profile=pt_BR system build --description $BASEDIR --target-dir $USERHOME/tmp-kamarada
+kiwi-ng result bundle --target-dir $USERHOME/tmp-kamarada --id pt_BR-Build1.1 --bundle-dir $BASEDIR
 chmod 777 ./Linux_Kamarada*
 chown $USERNAME ./Linux_Kamarada*
-rm -rf /tmp/myimage/Linux_Kamarada*
+rm -rf $USERHOME/tmp-kamarada/Linux_Kamarada*
